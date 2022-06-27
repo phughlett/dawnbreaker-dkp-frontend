5c94eccba3c1c7e8e7c5a53cf63fa45870cf6ba3
Patrick Hughlett <phughlett87@gmail.com>
2022-06-27 01:21:43 -0600

    made turn textfield disabled and added copy on click

--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Box, Grid, Stack, Button, TextField } from "@mui/material";
+import { Box, Grid, Stack, Button, TextField, Typography} from "@mui/material";
 import {useState} from 'react'
 
 function App() {
 
   const [sessionData, setSessionData] = useState({})
+  const [copyState, setCopyState] = useState('');
+  const [addonInit, setAddonInit] = useState('')
 
 
   function initializeSession(sessionData) {
     let body = {sessionData, action: 'CREATE'};
 
     body = JSON.stringify(body);
-
-
-
-
     fetch(`http://localhost:8080/session`, {
       method: 'POST',
       headers: {
@@ -26,6 +24,12 @@ function App() {
 
   }
 
+  function copyText(e){
+    navigator.clipboard.writeText(e.target.value)
+    setCopyState('Copied to Clipboard!');
+    setTimeout(() => setCopyState(''), 5000)
+  }
+
   return (
     <Box sx={{ margin: "5rem" }}>
       <Grid container spacing={2}>
@@ -33,7 +37,8 @@ function App() {
             <Stack spacing={1.5}>
               <TextField onChange={(e) => setSessionData(e.target.value)} variant="outlined" label={"Start/Stop Session"} />
               <Button onClick={() => initializeSession(sessionData)} variant="outlined">Start Session</Button>
-              <TextField label={"Return String from Backend for addon"} />
+              <TextField disabled onClick={(e) => copyText(e)} label={"Click to Copy"} value={addonInit} />
+              <Typography variant='caption'>{copyState}</Typography>
             </Stack>
         </Grid>
         <Grid item xs={4}>
