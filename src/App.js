e070c0c9388483ce18beffc85fa4dbb6213936b2
Patrick Hughlett <phughlett87@gmail.com>
2022-06-26 23:58:15 -0600

    built basic textboxes to test with backend

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Box, Grid, Stack, Button, TextField } from "@mui/material";
+import {useState} from 'react'
 
 function App() {
+
+  const [sessionData, setSessionData] = useState({})
+
+
+  function initializeSession(sessionData) {
+    let body = {sessionData, action: 'CREATE'};
+
+    body = JSON.stringify(body);
+
+
+
+
+    fetch(`http://localhost:8080/session`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body
+    })
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box sx={{ margin: "5rem" }}>
+      <Grid container spacing={2}>
+        <Grid item xs={4}>
+          <form>
+            <Stack spacing={1.5}>
+              <TextField onChange={(e) => setSessionData(e.target.value)} variant="outlined" label={"Start/Stop Session"} />
+              <Button onClick={() => initializeSession(sessionData)} variant="outlined">Start Session</Button>
+              <TextField label={"Return String from Backend for addon"} />
+            </Stack>
+          </form>
+        </Grid>
+        <Grid item xs={4}>
+          <Stack spacing={1.5}>
+            <TextField variant="outlined" label={"Add Item to Session"} />
+            <Button variant="outlined">Add Item</Button>
+          </Stack>
+        </Grid>
+        <Grid item xs={4}>
+          <Stack spacing={1.5}>
+            <TextField variant="outlined" label={"Initialize Session"} />
+            <Button variant="outlined">Start Session</Button>
+          </Stack>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
 
