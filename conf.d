04946dbe439ee9ac0514fedaf85b9b4ef24fac66
Patrick Hughlett <phughlett87@gmail.com>
2022-09-24 19:27:34 -0600

    conf changes

--- conf.d
+++ conf.d
@@ -1,15 +1,10 @@
 server {
     listen 80;
-    server_name dawnbreaker.app www.dawnbreaker.app;
-    return 301 https://dawnbreaker.app$request_uri;
-}
-
-server {
     listen 443 ssl;
     server_name www.dawnbreaker.app;
 
     ssl_certificate /etc/nginx/ssl/cert.pem;
-    ssl_certificate /etc/nginx/ssl/key.pem;
+    ssl_certificate_key /etc/nginx/ssl/key.pem;
 
     # . . . other code
     location /{
