5d24ccf85a5b801ee5226874a8dd61edaa6a8e6e
Patrick Hughlett <phughlett87@gmail.com>
2022-09-24 22:24:01 -0600

    server for router

--- server.js
+++ server.js
@@ -4,6 +4,11 @@ const app = express();
 
 app.use(express.static('./build'))
 
+
+app.get('/*', function(req,res) {
+  res.sendFile(path.join('./build', 'index.html'));
+});
+
 app.listen(80, () =>{
  console.log('Listening on 80')
 })
