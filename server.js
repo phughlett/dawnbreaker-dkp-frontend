2e951fb46c0d69f7a0096545ec31a3478165e6cb
Patrick Hughlett <phughlett87@gmail.com>
2022-09-24 23:29:50 -0600

    reverted express

--- server.js
+++ server.js
@@ -1,13 +1,5 @@
 const express =require('express')
-const https = require('https');
-const fs = require('fs');
 
-var key = fs.readFileSync('/app/selfsigned.key');
-var cert = fs.readFileSync('/app/selfsigned.crt');
-var options = {
-  key: key,
-  cert: cert
-};
 
 const app = express();
 
@@ -18,8 +10,8 @@ app.get('/*', function(req,res) {
   res.sendFile('/app/build/index.html');
 });
 
-var server = https.createServer(options, app);
 
-server.listen(80, () =>{
+
+app.listen(80, () =>{
  console.log('Listening on 80')
 })
