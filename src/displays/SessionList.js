f6d35c6d1f710b006087042ebe1c6804a41c2602
Patrick Hughlett <phughlett87@gmail.com>
2022-10-07 01:22:44 -0600

    added simple links to other pages

--- src/displays/SessionList.js
+++ src/displays/SessionList.js
@@ -4,6 +4,8 @@ import StartSession from '../components/raidsession/StartSession'
 import AddRaidTeam from '../components/raidTeam/AddRaidTeam'
 import AddonInit from '../components/addonInit/AddonInit'
 import AppContext from '../contexts/AppContext';
+import { Link } from "react-router-dom";
+
 import { Box, Grid } from "@mui/material";
 
 export default function SessionList(){
@@ -18,6 +20,8 @@ export default function SessionList(){
 
   return(
     <Box sx={{ margin: "5rem" }}>
+      <li><Link to={'/ledger'}>Ledger</Link></li>
+      <li><Link style={{leftMargin:'5rem'}} to={'/characters'}>Characters</Link></li>
     <h1>Sessions</h1>
     <Grid container spacing={2}>
       <Grid item xs={4}>
