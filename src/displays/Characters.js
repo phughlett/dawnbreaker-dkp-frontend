e8d66134d5732e01ed102e56d2aaff375798b40f
Patrick Hughlett <phughlett87@gmail.com>
2023-05-20 00:13:38 -0600

    Added unassign as raid option

--- src/displays/Characters.js
+++ src/displays/Characters.js
@@ -1,41 +1,39 @@
-import {useState, useEffect, useContext} from 'react';
-import { DataGrid  } from '@mui/x-data-grid';
-import AppContext from '../contexts/AppContext'
+import { useState, useEffect, useContext } from "react";
+import { DataGrid } from "@mui/x-data-grid";
+import AppContext from "../contexts/AppContext";
 import { Box, Grid, Stack, Button } from "@mui/material";
-import NavBar from '../components/appbar/NavBar'
-
+import NavBar from "../components/appbar/NavBar";
 
 export default function Characters() {
-  let {API, characters, getCharacters, setSession, navigate,raidTeams, getRaidTeams, setCharacters} = useContext(AppContext);
+  let {
+    API,
+    characters,
+    getCharacters,
+    setSession,
+    navigate,
+    raidTeams,
+    getRaidTeams,
+    setCharacters,
+  } = useContext(AppContext);
   const [sessionData, setSessionData] = useState([]);
-
-
-
 
   useEffect(() => {
     getCharacters();
     getRaidTeams();
-  }, [])
+  }, []);
 
-
-
-   function raidTeamNameGetter(id){
-
-    let match = raidTeams.filter((team) => team.id === id )
-    if(match.length === 0){
-     return '';
-    }else{
-     return match[0].name;
+  function raidTeamNameGetter(id) {
+    let match = raidTeams.filter((team) => team.id === id);
+    if (match.length === 0) {
+      return "";
+    } else {
+      return match[0].name;
     }
+  }
 
-   }
-
-   function actionButtons(update){
-
-
+  function actionButtons(update) {
     const postUpdate = (update) => {
-
-      let body = {update};
+      let body = { update };
 
       body = JSON.stringify(body);
 
@@ -44,47 +42,92 @@ export default function Characters() {
         headers: {
           "Content-Type": "application/json",
         },
-        body
+        body,
       })
         .then((response) => response.json())
         .then((data) => setCharacters(data))
         .catch((err) => console.log(err));
-
-    }
-    return(
-    <>
-   <Button size='small' variant='contained' onClick={()=>postUpdate(update)}>Send Update</Button>
-    </>
-    )
+    };
+    return (
+      <>
+        <Button
+          size="small"
+          variant="contained"
+          onClick={() => postUpdate(update)}
+        >
+          Send Update
+        </Button>
+      </>
+    );
   }
 
-  let classOptions = ['Death Knight', 'Warrior', 'Paladin', 'Rogue', 'Hunter', 'Shaman', 'Druid', 'Mage', 'Priest', 'Warlock'];
-  let roleOptions = ['Tank', 'DPS', 'Healer'];
+  let classOptions = [
+    "Death Knight",
+    "Warrior",
+    "Paladin",
+    "Rogue",
+    "Hunter",
+    "Shaman",
+    "Druid",
+    "Mage",
+    "Priest",
+    "Warlock",
+  ];
+  let roleOptions = ["Tank", "DPS", "Healer"];
+
+  let raidOptions = raidTeams.map((team) => {
+    return { value: team.id, label: team.name };
+  });
+  raidOptions.push({ value: null, label: "UNASSIGN" });
 
   let columns = [
-    {field: 'raid_team',
-    type: 'singleSelect',
-     headerName: 'Raid Team',
-     width: 200,
-     editable: true,
-     valueGetter: value => raidTeamNameGetter(value.value),
-     valueOptions: raidTeams.map(team => {
-      return {value: team.id, label: team.name}
-    })},
-    {field: 'name', headerName: 'Name', width: 200},
-    {field: 'dkp', headerName: 'DKP', width: 75},
-    {field: 'characterClass', type: 'singleSelect', headerName: 'Class', width: 75, editable: true, valueOptions: classOptions},
-    {field: 'role', type: 'singleSelect', headerName: 'Role', width: 75, editable:true, valueOptions: roleOptions},
-    {field: 'actions', headerName: 'Actions', width: 200, renderCell: params => actionButtons(params.row)}
+    {
+      field: "raid_team",
+      type: "singleSelect",
+      headerName: "Raid Team",
+      width: 200,
+      editable: true,
+      valueGetter: (value) => raidTeamNameGetter(value.value),
+      valueOptions: raidOptions,
+    },
+    { field: "name", headerName: "Name", width: 200 },
+    { field: "dkp", headerName: "DKP", width: 75 },
+    {
+      field: "characterClass",
+      type: "singleSelect",
+      headerName: "Class",
+      width: 75,
+      editable: true,
+      valueOptions: classOptions,
+    },
+    {
+      field: "role",
+      type: "singleSelect",
+      headerName: "Role",
+      width: 75,
+      editable: true,
+      valueOptions: roleOptions,
+    },
+    {
+      field: "actions",
+      headerName: "Actions",
+      width: 200,
+      renderCell: (params) => actionButtons(params.row),
+    },
   ];
-
 
   return (
     <>
-    <NavBar pageName={'Characters'}/>
-    <div style={{ height: 800, width: '100%' }}>
-    <DataGrid rows={characters} columns={columns} initialState={{sorting: {sortModel:[{field: 'raid_team', sort: 'asc'}]}}} />
-    </div>
+      <NavBar pageName={"Characters"} />
+      <div style={{ height: 800, width: "100%" }}>
+        <DataGrid
+          rows={characters}
+          columns={columns}
+          initialState={{
+            sorting: { sortModel: [{ field: "raid_team", sort: "asc" }] },
+          }}
+        />
+      </div>
     </>
   );
 }
