afc887409027e4bc1bc5bcdc29aafaee442d66df
Patrick Hughlett <phughlett87@gmail.com>
2023-05-29 14:30:11 -0600

    added navigate after logout

--- src/components/button/LogoutButton.js
+++ src/components/button/LogoutButton.js
@@ -7,12 +7,13 @@ import { useContext } from "react";
 export default function LogoutButton() {
 
 
-  let { setAdmin } = useContext(AppContext);
+  let { setAdmin, navigate } = useContext(AppContext);
 
 
   const handleLogout = () => {
     setAdmin(null)
     document.cookie = "admin=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
+    navigate("/")
   };
 
   return (
