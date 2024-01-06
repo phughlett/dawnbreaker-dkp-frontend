c89a7638b66e12b83f9094bbbad9f92c385b3d8b
Patrick Hughlett <phughlett87@gmail.com>
2024-01-06 16:22:49 -0700

    used pallete in table for hover

--- src/components/home/HomeTable.js
+++ src/components/home/HomeTable.js
@@ -7,79 +7,79 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import Paper from "@mui/material/Paper";
 import { Box, Typography } from "@mui/material";
-import AppContext from '../../contexts/AppContext';
+import AppContext from "../../contexts/AppContext";
+import { useTheme } from "@mui/styles";
 
 export default function HomeTable(data) {
+  const theme = useTheme();
   const [rows, setRows] = useState([]);
-  const [update, setUpdate] = useState(false)
-  const [hover, setHover] = useState(false)
-  let {API, raidTeams, navigate} = useContext(AppContext);
+  let { API, navigate } = useContext(AppContext);
   const raid = data.raidTeam;
 
-
-
   useEffect(() => {
-
     const fetchData = () => {
       fetch(`${API}/characters/team/${raid.id}`, {
         method: "GET",
       })
         .then((response) => response.json())
         .then((data) => {
-          let sortedData = data.sort((a,b) => {
-            return b.dkp - a.dkp
-          })
+          let sortedData = data.sort((a, b) => {
+            return b.dkp - a.dkp;
+          });
 
-          setRows(sortedData)
+          setRows(sortedData);
         })
         .catch((err) => console.log(err));
-
-    }
+    };
     const interval = setInterval(() => {
-      fetchData()
-    }, 15000)
+      fetchData();
+    }, 15000);
 
-    fetchData()
+    fetchData();
 
     return () => clearInterval(interval);
-
-
-  }, []);
+  }, [API, raid.id]);
 
   const navigateOnClick = (charName) => {
-    navigate(`/${charName}`)
-  }
-
+    navigate(`/${charName}`);
+  };
 
   return (
-    <Box sx={{textAlign: 'center', maxWidth: 400}}>
-    <Typography  variant="h4">{raid.name}</Typography>
-    <TableContainer align="center" component={Paper}>
-      <Table sx={{ minWidth: 50, maxWidth: 250, "& .MuiTableRow-root:hover": {
-      backgroundColor: "rgba(255, 255, 255, 0.08)",
-      cursor: 'pointer'
-    } }} aria-label="simple table">
-        <TableHead>
-          <TableRow>
-            <TableCell align="center">Character</TableCell>
-            <TableCell align="center">DKP</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {rows.map((row) => (
-            <TableRow
-              key={row.name}
-              onClick={() => navigateOnClick(row.name)}
-            >
-              <TableCell align="center" component="th" scope="row">
-                {row.name}
-              </TableCell>
-              <TableCell align="center">{row.dkp}</TableCell>
+    <Box sx={{ textAlign: "center", maxWidth: 400 }}>
+      <Typography variant="h4">{raid.name}</Typography>
+      <TableContainer align="center" component={Paper}>
+        <Table
+          sx={{
+            minWidth: 50,
+            maxWidth: 250,
+            "& .MuiTableRow-root:hover": {
+              backgroundColor: theme.palette.action.hover,
+              cursor: "pointer",
+            },
+          }}
+          aria-label="simple table"
+        >
+          <TableHead>
+            <TableRow>
+              <TableCell align="center">Character</TableCell>
+              <TableCell align="center">DKP</TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
+          </TableHead>
+          <TableBody>
+            {rows.map((row) => (
+              <TableRow
+                key={row.name}
+                onClick={() => navigateOnClick(row.name)}
+              >
+                <TableCell align="center" component="th" scope="row">
+                  {row.name}
+                </TableCell>
+                <TableCell align="center">{row.dkp}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
     </Box>
   );
 }
